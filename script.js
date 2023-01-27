baf48f8a078eb6e5f4a73345793ba3ab69de31c9
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2023-01-27 16:48:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+/* Slider Part */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+/********************************************************************/
+
+/* FAQ Part */
+
+const qs=document.querySelectorAll('#left-faq h1');
+
+
+function toggle_testers(div_name)
+{
+if(document.getElementById(div_name).style.display == 'none')
+  document.getElementById(div_name).style.display = 'block';
+ else
+  document.getElementById(div_name).style.display = 'none';
+}
